344da130804e9a4abc6bc57a621d6ecba5acea72
maerli <maerli.pereira@gmail.com>
2018-04-17 10:57:42 -0300

    ajustando exemplos

---
+++ sistemas-de-equações/equacoes01.js
@@ -0,0 +1,20 @@
+const EquationMaker = require('.././equation_maker.js');
+const ArrayEquation = EquationMaker.ArrayEquation;
+//let eqs = new ArrayEquation(3,3);
+//rezolvendo o systema
+//  3x - 5y + 3z =  1
+// -4x + 2y +  z = -2
+//  x  -  y -  z =  2
+
+let coef =   [
+    [3, -5, 3],
+    [-4, 2, 1],
+    [1, -1, -1]
+];
+
+let res = [1,-2,2];
+
+let eqs = new ArrayEquation(coef,res);
+//console.log(eqs);
+let solve = eqs.solveAll();
+eqs.toString();
